a0813bd2bcb32574577ca13c1934c42833880732
slava.front <viacheslav.front.bsx@gmail.com>
2023-07-23 12:17:06 +0300

    update config

--- src/app/modules/shared-module/shared.module.ts
+++ src/app/modules/shared-module/shared.module.ts
@@ -2,11 +2,11 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FooterComponent} from "./components/footer/footer.component";
 import {HttpClientModule} from "@angular/common/http";
-import {MatLegacySnackBarModule as MatSnackBarModule} from "@angular/material/legacy-snack-bar";
-import {MatLegacyCardModule as MatCardModule} from "@angular/material/legacy-card";
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
-import {MatLegacyFormFieldModule as MatFormFieldModule} from "@angular/material/legacy-form-field";
-import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
+import {MatSnackBarModule as MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatCardModule as MatCardModule} from "@angular/material/card";
+import {MatButtonModule as MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule as MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule as MatInputModule} from "@angular/material/input";
 import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
