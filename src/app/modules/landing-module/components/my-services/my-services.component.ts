ef7c43f72db64a01331d23e9b0a94a69a62167c5
chopyk89 <if040489cvv@gmail.com>
2024-01-24 21:42:31 +0200

    init commit

--- src/app/modules/landing-module/components/my-services/my-services.component.ts
+++ src/app/modules/landing-module/components/my-services/my-services.component.ts
@@ -17,21 +17,21 @@ export class MyServicesComponent implements OnInit {
     }[] = [
         {
             title: "Web Apps and Portals",
-            imgUrl: "https://chopyk89.github.io/portfolio/assets/images/responsive-web-design-icon.png",
+            imgUrl: "https://Viacheslav-Chopyk.github.io/portfolio/assets/images/responsive-web-design-icon.png",
             description:
                 "<h4>Creating Web App and Portals with Angular, Vanilla JS:</h4></h4> <p>- Cross-Device,</p> <p>- Responsive design,</p> <p>- Cross-Browser,</p> <p>- High-Quality software</p>",
             cardDetailsShow: false,
         },
         {
             title: "Mobile Apps",
-            imgUrl: "https://chopyk89.github.io/portfolio/assets/images/cross-platform.png",
+            imgUrl: "https://Viacheslav-Chopyk.github.io/portfolio/assets/images/cross-platform.png",
             description:
                 "<h4>Flutter App Development:</h4> <p>- Cross-Device,</p> <p>- Best performance,</p> <p>- Custom Flutter Apps From Scratch,</p> <p>- Beautiful User Interface on any screen</p>",
             cardDetailsShow: false,
         },
         {
             title: "Support",
-            imgUrl: "https://chopyk89.github.io/portfolio/assets/images/support-icon.png",
+            imgUrl: "https://Viacheslav-Chopyk.github.io/portfolio/assets/images/support-icon.png",
             description:
                 "<h4>Support all systems:</h4> <p>- Web Applications,</p> <p>- Mobile Applications,</p> <p>- Create new functional</p>",
             cardDetailsShow: false,
